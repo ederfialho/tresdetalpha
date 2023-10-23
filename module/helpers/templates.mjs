90cff433d4143af7173c5d1510972e159a07f982
ederfialho <dzrmail@gmail.com>
2023-10-23 00:14:12 -0300

    Corrigindo o erro que apagou a lista de itens nas abas da ficha de personagem.

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -7,14 +7,14 @@
   return loadTemplates([
 
     // Actor partials.
-    "systems/tresdetalpha/templates/actor/parts/actor-features.html",
-    "systems/tresdetalpha/templates/actor/parts/actor-vantagens.html",
     "systems/tresdetalpha/templates/actor/parts/actor-desvantagens.html",
-    "systems/tresdetalpha/templates/actor/parts/actor-vantagemunica.html",
-    "systems/tresdetalpha/templates/actor/parts/actor-pericias.html",
+    "systems/tresdetalpha/templates/actor/parts/actor-effects.html",
+    "systems/tresdetalpha/templates/actor/parts/actor-features.html",
     "systems/tresdetalpha/templates/actor/parts/actor-items.html",
     "systems/tresdetalpha/templates/actor/parts/actor-magias.html",
+    "systems/tresdetalpha/templates/actor/parts/actor-pericias.html",
     "systems/tresdetalpha/templates/actor/parts/actor-spells.html",
-    "systems/tresdetalpha/templates/actor/parts/actor-effects.html",
+    "systems/tresdetalpha/templates/actor/parts/actor-vantagemunica.html",
+    "systems/tresdetalpha/templates/actor/parts/actor-vantagens.html",   
   ]);
 };
